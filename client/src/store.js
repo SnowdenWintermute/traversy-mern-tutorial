01d4377103d737a0797d1271c8b2c3e59156722d
SnowdenWintermute <michael.p.silverman@gmail.com>
2018-11-19 15:07:55 -0500

    Profile creation form

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import thunk from "redux-thunk"; //something to do with allowing us to use dispatch in action creators without specifically importing thunk in every file
 import rootReducer from "./reducers"; //This gets the combined reducers from index.js in reducers folder
 
 const initialState = {};
