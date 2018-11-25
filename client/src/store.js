4d7fe3b8f7af7b36fb493a93183288f3dd45bfe7
SnowdenWintermute <michael.p.silverman@gmail.com>
2018-11-25 11:08:40 -0500

    Lecture 55

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk"; //something to do with allowing us to use dispatch in action creators without specifically importing thunk in every file
 import rootReducer from "./reducers"; //This gets the combined reducers from index.js in reducers folder
 
-const initialState = {};
+const initialState = {
+  errors: {}
+};
 
 const middleware = [thunk];
 
